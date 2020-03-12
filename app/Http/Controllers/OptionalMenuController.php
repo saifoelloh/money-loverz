783ac5c8ce201a88aee9f611e943470e7e83c29a
saifoelloh <saifoelloh@gmail.com>
2020-03-12 23:51:16 +0700

    creating optional menus

---
+++ app/Http/Controllers/OptionalMenuController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OptionalMenu;
+use Illuminate\Http\Request;
+
+class OptionalMenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OptionalMenu  $optionalMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OptionalMenu $optionalMenu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OptionalMenu  $optionalMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OptionalMenu $optionalMenu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OptionalMenu  $optionalMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OptionalMenu $optionalMenu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OptionalMenu  $optionalMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OptionalMenu $optionalMenu)
+    {
+        //
+    }
+}
---
+++ app/OptionalMenu.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OptionalMenu extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+      'name',
+    ]
+}
