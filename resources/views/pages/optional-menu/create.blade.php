366bc27d3b63d382ab492a9853cb92ab6a814774
saifoelloh <saifoelloh@gmail.com>
2020-03-14 23:21:41 +0700

    order-menu implementation

---
+++ resources/views/pages/optional-menu/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app', ['title' => __('Adding Menu')])
+
+@section('content')
+@include('users.partials.header', ['title' => __('Tambah Optional Menu')])
+
+<div class="container-fluid mt--7">
+    <div class="row">
+        <div class="col-xs-12 col-md-5">
+            <div class="card shadow">
+                <div class="card-body">
+                    <div class="card-title">
+                        <div class="row">
+                            <div class="col-6">
+                                Tambah Optional Menu
+                            </div>
+                            <div class="col-6 text-right">
+                                <a class="btn btn-icon btn-primary btn-sm" href="{{ route('menu.show', $menu->id) }}">
+                                    <span class="btn-inner--text">kembali</span>
+                                    <span class="btn-inner--icon"><i class="fas fa-reply"></i></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <form action="{{ route('optional-menu.store', [
+                        'menu_id' => $menu->id
+                        ]) }}" method="post" accept-charset="utf-8">
+                        @csrf
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label class="form-label" for="name">Nama</label>
+                                    <input class="form-control" type="text" name="name" id="name" placeholder="nama opsi pilihan" />
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="category">Kategori</label>
+                                    <input class="form-control" name="category" id="category" placeholder="nasi" />
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="price">Harga</label>
+                                    <input class="form-control" type="number" name="price" id="price" placeholder="harga makanan" />
+                                </div>
+                                <div class="row">
+                                    <div class="col text-right">
+                                        <button class="btn btn-icon btn-warning" type="cancel">
+                                            <span class="btn-inner--icon"><i class="fas fa-times"></i></span>
+                                            <span class="btn-inner--text">cancel</span>
+                                        </button>
+                                        <button class="btn btn-icon btn-success" type="submit">
+                                            <span class="btn-inner--icon"><i class="fas fa-plus"></i></span>
+                                            <span class="btn-inner--text">submit</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('layouts.footers.auth')
+</div>
+@endsection
---
+++ resources/views/pages/optional-menu/index.blade.php
@@ -0,0 +1,56 @@
+<div class="card shadow">
+    <div class="card-body">
+        <div class="card-title">
+            <div class="row">
+                <div class="col-6">
+                    Daftar Optional Menu
+                </div>
+                <div class="col-6 text-right">
+                    <a class="btn btn-icon btn-success btn-sm" href="{{ route('optional-menu.create', $menu_id) }}">
+                        <span class="btn-inner--icon"><i class="fas fa-plus"></i></span>
+                        <span class="btn-inner--text">tambah</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="table-responsive">
+            <div>
+                <table class="table align-items-center">
+                    <thead class="thead-light">
+                        <tr class="text-center">
+                            <th scope="col" class="sort" data-sort="name">No</th>
+                            <th scope="col" class="sort" data-sort="budget">Nama</th>
+                            <th scope="col" class="sort" data-sort="budget">Kategori</th>
+                            <th scope="col" class="sort" data-sort="status">Harga</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody class="list">
+                        @foreach($options as $item)
+                        <tr class="text-center">
+                            <th> {{ $item->id }} </th>
+                            <td> {{ $item->name }} </td>
+                            <td> {{ $item->category }} </td>
+                            <td> {{"Rp. ".number_format($item->price, 0)}} </td>
+                            <td>
+                                <a class="btn btn-icon btn-warning btn-sm" href="{{ route('optional-menu.edit', $item->id) }}">
+                                    <span class="btn-inner--icon"><i class="fas fa-edit"></i></span>
+                                    <span class="btn-inner--text">edit</span>
+                                </a>
+                                <form action="{{ route('optional-menu.destroy', $item->id) }}" method="post" class="my-0 d-inline">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button class="btn btn-icon btn-danger btn-sm" type="submit">
+                                        <span class="btn-inner--icon"><i class="fas fa-trash"></i></span>
+                                        <span class="btn-inner--text">delete</span>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
