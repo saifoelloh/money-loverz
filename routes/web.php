d40da9f3fec35da9e2fc29c80198865b6f2d6b10
saifoelloh <saifoelloh@gmail.com>
2020-03-14 23:22:24 +0700

    implement menu and optional-menu into route

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('menu', 'MenuController');
-Route::resource('optional-menu', 'OptionalMenuController');
 
 Route::group(['middleware' => 'auth'], function () {
+	Route::resource('admin', 'UserController');
+	Route::resource('menu', 'MenuController');
+	Route::resource('optional-menu', 'OptionalMenuController');
+	Route::group([
+		'as' => 'optional-menu',
+		'prefix' => 'optional-menu'
+	], function () {
+		Route::get('/create/{id}', 'OptionalMenuController@create')->name('.create');
+	});
 	Route::resource('user', 'UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
 });
-
