a91bcab8711b46dc665630a9cadda4337ee071cc
saifoelloh <saifoelloh@gmail.com>
2020-03-14 23:22:08 +0700

    refactoring code

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,88 +1,88 @@
 @extends('layouts.app', ['title' => __('User Management')])
 
 @section('content')
-    @include('layouts.headers.cards')
-
-    <div class="container-fluid mt--7">
-        <div class="row">
-            <div class="col">
-                <div class="card shadow">
-                    <div class="card-header border-0">
-                        <div class="row align-items-center">
-                            <div class="col-8">
-                                <h3 class="mb-0">{{ __('Users') }}</h3>
-                            </div>
-                            <div class="col-4 text-right">
-                                <a href="{{ route('user.create') }}" class="btn btn-sm btn-primary">{{ __('Add user') }}</a>
-                            </div>
+@include('users.partials.header', ['title' => __('Daftar Admin')])
+<div class="container-fluid mt--7">
+    <div class="row">
+        <div class="col">
+            <div class="card shadow">
+                <div class="card-header border-0">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class="mb-0">{{ __('Users') }}</h3>
+                        </div>
+                        <div class="col-4 text-right">
+                            <a href="{{ route('user.create') }}" class="btn btn-sm btn-primary">{{ __('Add user') }}</a>
                         </div>
                     </div>
-                    
-                    <div class="col-12">
-                        @if (session('status'))
-                            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                {{ session('status') }}
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                        @endif
-                    </div>
+                </div>
 
+                <div class="col-12">
+                    @if (session('status'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('status') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                </div>
+
+                <div class="card-body">
                     <div class="table-responsive">
                         <table class="table align-items-center table-flush">
                             <thead class="thead-light">
-                                <tr>
+                                <tr class="text-center">
+                                    <th scope="col">No</th>
                                     <th scope="col">{{ __('Name') }}</th>
                                     <th scope="col">{{ __('Email') }}</th>
                                     <th scope="col">{{ __('Creation Date') }}</th>
-                                    <th scope="col"></th>
+                                    <th scope="col">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($users as $user)
-                                    <tr>
-                                        <td>{{ $user->name }}</td>
-                                        <td>
-                                            <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
-                                        </td>
-                                        <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
-                                        <td class="text-right">
-                                            <div class="dropdown">
-                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <i class="fas fa-ellipsis-v"></i>
-                                                </a>
-                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                    @if ($user->id != auth()->id())
-                                                        <form action="{{ route('user.destroy', $user) }}" method="post">
-                                                            @csrf
-                                                            @method('delete')
-                                                            
-                                                            <a class="dropdown-item" href="{{ route('user.edit', $user) }}">{{ __('Edit') }}</a>
-                                                            <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this user?") }}') ? this.parentElement.submit() : ''">
-                                                                {{ __('Delete') }}
-                                                            </button>
-                                                        </form>    
-                                                    @else
-                                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Edit') }}</a>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                <tr class="text-center">
+                                    <td>{{ $user->id }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>
+                                        <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
+                                    </td>
+                                    <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
+                                    <td>
+                                        <a class="btn btn-icon btn-success btn-sm" href="{{ route('admin.show', $user->id) }}">
+                                            <span class="btn-inner--icon"><i class="fas fa-eye"></i></span>
+                                            <span class="btn-inner--text">detail</span>
+                                        </a>
+                                        <a class="btn btn-icon btn-warning btn-sm" href="{{ route('admin.edit', $user->id) }}">
+                                            <span class="btn-inner--icon"><i class="fas fa-edit"></i></span>
+                                            <span class="btn-inner--text">edit</span>
+                                        </a>
+                                        <form action="{{ route('user.destroy', $user->id) }}" method="post" class="my-0 d-inline">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="btn btn-icon btn-danger btn-sm" type="submit">
+                                                <span class="btn-inner--icon"><i class="fas fa-trash"></i></span>
+                                                <span class="btn-inner--text">delete</span>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
-                    <div class="card-footer py-4">
-                        <nav class="d-flex justify-content-end" aria-label="...">
-                            {{ $users->links() }}
-                        </nav>
-                    </div>
+                </div>
+
+                <div class="card-footer py-4">
+                    <nav class="d-flex justify-content-end" aria-label="...">
+                        {{ $users->links() }}
+                    </nav>
                 </div>
             </div>
         </div>
-            
-        @include('layouts.footers.auth')
     </div>
+
+    @include('layouts.footers.auth')
+</div>
 @endsection
