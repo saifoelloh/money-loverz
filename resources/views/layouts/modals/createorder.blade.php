b1adf82ecaa4df7c66c03a603670980cc00bcc5a
saifoelloh <saifoelloh@gmail.com>
2020-04-12 19:48:46 +0700

    connecting FE to BE

--- resources/views/layouts/modals/createorder.blade.php
+++ resources/views/layouts/modals/createorder.blade.php
@@ -1,15 +1,19 @@
 <div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-labelledby="modal-notification" style="display: none;" aria-hidden="true">
   <div class="modal-dialog modal-danger modal-dialog-centered modal-" role="document">
     <div class="modal-content bg-gradient-info">
-      <div class="modal-header">
-        <h6 class="modal-title" id="modal-title-notification">Daftar untuk buat pesanan baru.</h6>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form action="{{ route('make-order.store')}}" method="POST">
-          @csrf
+
+      <!-- START Form -->
+      <form action="{{ route('make-order.store') }}" method="POST">
+        @csrf
+        <div class="modal-header">
+          <h6 class="modal-title" id="modal-title-notification">Daftar untuk buat pesanan baru.</h6>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+
+        <!-- START Modal-Body -->
+        <div class="modal-body">
           <div class="form-group">
             <label for="name" class="form-label">Nama</label>
             <div class="input-group input-group-alternative">
@@ -19,22 +23,51 @@
               <input class="form-control" placeholder="John Smith" type="text" name="name">
             </div>
           </div>
-          <div class="form-group">
-            <label for="phone" class="form-label">No Telepon</label>
-            <div class="input-group input-group-alternative">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-phone"></i></span>
+          <div class="form-group row">
+            <div class="col">
+              <label for="phone" class="form-label">No Telepon</label>
+              <div class="input-group input-group-alternative">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                </div>
+                <input class="form-control" placeholder="08xxxx" type="text" name="phone">
               </div>
-              <input class="form-control" placeholder="08xxxx" type="email" name="email">
+            </div>
+            <div class="col">
+              <label for="email" class="form-label">Email</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                </div>
+                <input class="form-control" placeholder="Email" type="email" name="email">
+              </div>
             </div>
           </div>
           <div class="form-group">
-            <label for="email" class="form-label">Email</label>
+            <label for="gender" class="form-label">Gender</label>
+            <div class="input-group input-group-alternative">
+              <div class="input-group-prepend">
+                <span class="input-group-text"><i class="fas fa-venus"></i></span>
+              </div>
+              <select id="gender" class="form-control" name="gender">
+                <option value="male">Laki-laki</option>
+                <option value="female">Perempuan</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="kecamatan" class="form-label">Kecamatan</label>
             <div class="input-group">
               <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
               </div>
-              <input class="form-control" placeholder="Email" type="email" name="email">
+              <select id="kecamatan" class="form-control" name="kecamatan">
+                @foreach ($kecamatan as $item)
+                  <option value="{{ $item }}">
+                  {{ ucwords($item) }}
+                  </option>
+                @endforeach
+              </select>
             </div>
           </div>
           <div class="form-group">
@@ -45,19 +78,23 @@
               </div>
               <select id="package" class="form-control" name="package">
                 @foreach ($packages as $package)
-                <option value="{{ $package['id'] }}">
+                  <option value="{{ $package['id'] }}">
                   {{ $package['name'] }}
-                </option>
+                  </option>
                 @endforeach
               </select>
             </div>
           </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-link text-white">Input Kode Pesanan</button>
-        <a href="/make-order" type="button" class="btn btn-white ml-auto">Buat pesanan</a>
-      </div>
+        </div><!-- END Modal-Body -->
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-link text-white">Input Kode Pesanan</button>
+          <button type="submit" class="btn btn-white ml-auto">
+            Buat Pesanan
+          </button>
+        </div>
+      </form><!-- END Form -->
+
     </div>
   </div>
 </div>
