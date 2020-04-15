3bc957ce1c390237fcb22625d39bb64b6a337d0b
saifoelloh <saifoelloh@gmail.com>
2020-04-16 01:01:11 +0700

    relayouting and implementation

--- resources/views/pages/pdf/order.blade.php
+++ resources/views/pages/pdf/order.blade.php
@@ -8,34 +8,67 @@
   </head>
   <body>
     <div class="">
+
+      <table class="w-100 table-borderless">
+        <tr>
+          <td>
+            <span class="h5">
+              Invoice {{ $order->code }}
+            </span><br>
+            <small>
+              Tanggal Pesanan: {{ date_format(date_create($order->created_at), "l, d F Y") }}
+            </small>
+          </td>
+          <td>
+            <img src="http://localhost:8000/argon/img/brand/logo-black.png" alt=""/>
+          </td>
+        </tr>
+      </table>
+
+      <hr>
+
       <!-- START data transaksi -->
       <table class="w-100 table table-borderless">
         <tr>
           <td>
-            <ul class="list-unstyled">
-              <li>
-                {{ ucwords($order->user->name) }}
-              </li>
-              <li>
-                {{ $order->user->phone }}
-              </li>
-              <li>
-                {{ ucwords($order->kecamatan) }}
-              </li>
-              <li>
-                {{ ucwords($order->payment_method) }}
-              </li>
-            </ul>
+            <p class="font-weight-bold">
+              Informasi Pemesanan
+            </p>
+            <table class="table table-borderless">
+              <tr>
+                <td>
+                  Jenis Pesanan<br>
+                  Paket<br>
+                  Status<br>
+                  Pembayaran
+                </td>
+                <td>
+                  : Reguler (Lunch)<br>
+                  : Paket Coba (3 Item)<br>
+                  : Confirmed<br>
+                  : Transfer
+                </td>
+              </tr>
+            </table>
           </td>
-          <td class="text-right">
-            <ul class="list-unstyled">
-              <li>
-                {{ $order->code }}
-              </li>
-              <li>
-                Paket Coba | {{ $order->package->total_items }} Item
-              </li>
-            </ul>
+          <td>
+            <p class="font-weight-bold">
+              Informasi Pelanggan
+            </p>
+            <table class="table table-borderless">
+              <tr>
+                <td>
+                  Nama<br>
+                  Email<br>
+                  No Telp
+                </td>
+                <td>
+                  : Bang Gofur<br>
+                  : gofur@mail.co<br>
+                  : 0840174
+                </td>
+              </tr>
+            </table>
           </td>
         </tr>
       </table>
@@ -43,13 +76,13 @@
 
       <!-- START table of order -->
       <div class="float-none">
-        <table class="table table-borderless text-center">
+        <table class="table table-bordered text-center">
           <thead>
             <tr>
               <th>No</th>
               <th>Nama Menu</th>
-              <th>Tanggal Pengiriman</th>
               <th>Tambahan</th>
+              <th>Tanggal Pengiriman</th>
               <th>Jumlah</th>
               <th>Harga</th>
             </tr>
@@ -70,8 +103,8 @@
               <tr>
                 <td>{{ $key+1 }}</td>
                 <td>{{ $item->name }}</td>
-                <td>{{ $tanggal }}</td>
                 <td>{{ $item->pivot->optional }}</td>
+                <td>{{ $tanggal }}</td>
                 <td>{{ $item->pivot->total }}</td>
                 <td>{{ "Rp ".number_format($item->price) }}</td>
               </tr>
@@ -86,6 +119,27 @@
         </table>
       </div>
       <!-- END table of order -->
+
+      <table class="w-100 table-borderless">
+        <tr>
+          <td>
+            <span class="font-weight-bold">
+              Alamat Pengiriman
+            </span><br>
+            <small>
+              jl. Bayumanik , 123123, 123113 semarang.
+            </small>
+          </td>
+          <td class="text-right">
+            <span class="font-weight-bold">
+              Dikonfirmasi Oleh
+            </span><br>
+            <small>
+              Satu Pintu ({{ date_format(date_create($order->updated_at), "d/m/y, H:i") }})
+            </small>
+          </td>
+        </tr>
+      </table>
 
     </div>
   </body>
