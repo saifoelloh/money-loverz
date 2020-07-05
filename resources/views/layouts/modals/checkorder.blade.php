885929477c9e1bf7674aaf1eabdb06548f89df03
saifoelloh <saifoelloh@gmail.com>
2020-07-05 11:43:08 +0700

    change some layout

--- resources/views/layouts/modals/checkorder.blade.php
+++ resources/views/layouts/modals/checkorder.blade.php
@@ -7,13 +7,14 @@
               <div class="text-center text-muted mb-4">
                 Form Pemesanan.
               </div>
-              <form role="form"> <!-- FORM -->
+              <form role="form" action="{{route('list-order.find')}}" method="POST"> <!-- FORM -->
+                @csrf
                 <div class="form-group mb-3">
                   <div class="input-group">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-mobile-button"></i></span>
                     </div>
-                    <input class="form-control" placeholder="Nomor HP (WA aktif)" type="text">
+                    <input class="form-control" placeholder="Nomor HP (WA aktif)" type="text" name="phone">
                   </div>
                 </div>
                 <div class="form-group mb-3">
@@ -21,13 +22,15 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-email-83"></i></span>
                     </div>
-                    <input class="form-control" placeholder="Email" type="text">
+                    <input class="form-control" placeholder="Email" type="text" name="email">
                   </div>
                 </div>
                 <div class="text-center">
 
                   <!-- link ubah -->
-                  <a href="/list-order" type="button" class="btn btn-block btn-info my-4">Lanjut Order</a>
+                  <button type="submit" class="btn btn-block btn-info my-4">
+                    Lanjut Order
+                  </button>
                   
                 </div>
               </form>
--- resources/views/makeorder.blade.php
+++ resources/views/makeorder.blade.php
@@ -3,83 +3,82 @@
 @section('content')
   <div class="header bg-gradient-dark py-7 py-lg-8">
     <div class="page-header">
-      <div class="container">
-        <div class="card mx-auto">
-          <div class="card-body">
-            <h2 class="text-center">Pilih Menu</h2>
-            <table class="table table-borderless table-hover text-center">
-              <thead>
+      <div class="card mx-auto">
+        <div class="card-body">
+          <h2 class="text-center">Pilih Menu</h2>
+          <table class="table table-borderless table-hover text-center">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Nama Menu</th>
+                <th>Harga</th>
+                <th>Jumlah</th>
+                <th>Tanggal Pengiriman</th>
+                <th>Tambahan</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              @for ($i=1;$i<5;$i++)
                 <tr>
-                  <th>No</th>
-                  <th>Nama Menu</th>
-                  <th>Harga</th>
-                  <th>Jumlah</th>
-                  <th>Tanggal Pengiriman</th>
-                  <th>Tambahan</th>
-                  <th>Aksi</th>
+                  <td>{{ $i }}</td>
+                  <td>Nasi Mangkuk</td>
+                  <td>Rp 20.000</td>
+                  <td>1</td>
+                  <td>Senin, 20/04/2020</td>
+                  <td>-</td>
+                  <td>
+                    <form action="{{route('make-order.destroy', $i)}}">
+                      <button class="btn btn-outline-danger btn-sm">
+                        <i class="fas fa-trash"></i>
+                      </button>
+                    </form>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                @for ($i=1;$i<5;$i++)
-                  <tr>
-                    <td>{{ $i }}</td>
-                    <td>Nasi Mangkuk</td>
-                    <td>Rp 20.000</td>
-                    <td>1</td>
-                    <td>Senin, 20/04/2020</td>
-                    <td>-</td>
-                    <td>
-                      <form action="{{route('make-order.destroy', $i)}}">
-                        <button class="btn btn-outline-danger btn-sm">
-                          <i class="fas fa-trash"></i>
-                        </button>
-                      </form>
-                    </td>
-                  </tr>
-                @endfor
-                <!-- START Form -->
-                <form action="{{ route('make-order.add', $i) }}">
-                  <tr>
-                    <td>{{ $i }}</td>
-                    <td>
-                      <div class="form-group">
-                        <input class="form-control" placeholder="Pilih Menu" type="text" name="menu">
-                      </div>
-                    </td>
-                    <td>
-                      <div class="form-group">
-                        <input class="form-control" type="text" disabled>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="form-group">
-                        <input class="form-control" type="number" name="jumlah" min="0" {{ $i%2 ? 'disabled' : ''}}>
-                      </div>
-                    </td>
-                    <td>
-                      <div class="form-group">
-                        <input class="form-control" type="date" name="pengiriman">
-                      </div>
-                    </td>
-                    <td>
-                      <div class="form-group">
-                        <select id="option" name="option">
-                          <option value="">-</option>
-                          @foreach ($tambahan as $item)
-                            <option value="{{ $item }}">
-                              {{ ucwords($item) }}
-                            </option>
-                          @endforeach
-                        </select>
-                      </div>
-                    </td>
-                  </tr>
-                </form>
-                <!-- END Form -->
+              @endfor
+              <!-- START Form -->
+              <form action="{{ route('make-order.add', $i) }}" method="POST">
+                @csrf
+                <tr>
+                  <td>{{ $i }}</td>
+                  <td>
+                    <div class="form-group">
+                      <input class="form-control" placeholder="Pilih Menu" type="text" name="menu">
+                    </div>
+                  </td>
+                  <td>
+                    <div class="form-group">
+                      <input class="form-control" type="text" disabled>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="form-group">
+                      <input class="form-control" type="number" name="jumlah" min="0" {{ $i%2 ? 'disabled' : ''}}>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="form-group">
+                      <input class="form-control" type="date" name="pengiriman">
+                    </div>
+                  </td>
+                  <td>
+                    <div class="form-group">
+                      <select id="option" name="option">
+                        <option value="">-</option>
+                        @foreach ($tambahan as $item)
+                          <option value="{{ $item }}">
+                          {{ ucwords($item) }}
+                          </option>
+                        @endforeach
+                      </select>
+                    </div>
+                  </td>
+                </tr>
+              </form>
+              <!-- END Form -->
 
-              </tbody>
-            </table>
-          </div>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
