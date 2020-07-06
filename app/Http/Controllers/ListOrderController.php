54530d26120d3c28f71c25f7bf9df4ea3058db62
saifoelloh <saifoelloh@gmail.com>
2020-07-06 23:35:44 +0700

    add redirect when user is unregister

--- app/Http/Controllers/ListOrderController.php
+++ app/Http/Controllers/ListOrderController.php
@@ -14,16 +14,22 @@ class ListOrderController extends Controller
     public function find(Request $request)
     {
       $customer = User::where('phone', $request->phone)->first();
-      $packages = Package::all(['id', 'name']);
-      $order = new Order();
 
-      return view('listorder', [
-        'customer' => $customer,
-        'orders' => $customer->orders,
-        'count' => sizeof($customer->orders),
-        'packages' => $packages,
-        'order' => $order,
-      ]);
+      if ($customer) {
+        $packages = Package::all(['id', 'name']);
+        $order = new Order();
+
+        return view('listorder', [
+          'customer' => $customer,
+          'orders' => $customer->orders,
+          'count' => sizeof($customer->orders),
+          'packages' => $packages,
+          'order' => $order,
+        ]);
+      } else {
+        return redirect(route('landing-page'));
+      }
+
     }
 
 
