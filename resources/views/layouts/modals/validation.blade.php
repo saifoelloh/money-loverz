ccc52a649b27e964f4e7ac1bbd925421b0da9f8b
saifoelloh <saifoelloh@gmail.com>
2020-07-08 21:43:51 +0700

    create validation page

---
+++ resources/views/layouts/modals/validation.blade.php
@@ -0,0 +1,34 @@
+<div class="modal fade" id="modal-validation" tabindex="-1" role="dialog" aria-labelledby="modal-form" style="display: none;" aria-hidden="true">
+  <div class="modal-dialog modal-sm" role="document">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Pernah Order ?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body p-0">
+        <div class="card-body px-lg-5 py-lg-5">
+
+          <button class="btn btn-success btn-icon btn-block" data-toggle="modal" data-target="#modal-check">
+            <span class="btn-inner--icon">
+              <i class="fa fa-check"></i>
+            </span>
+            <span class="nav-link-inner--text">Sudah</span>
+          </button>
+
+          <button class="btn btn-info btn-icon btn-block" data-toggle="modal" data-target="#modal-form">
+            <span class="btn-inner--icon">
+              <i class="fa fa-times"></i>
+            </span>
+            <span class="nav-link-inner--text">Belum</span>
+          </button>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
